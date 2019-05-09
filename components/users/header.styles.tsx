2beebb3cf56db8c0666d7773a7daee94fe6528f0
Miguel Ángel Aguilar Peña <miguelangela@ocu.es>
2019-05-09 17:35:15 +0200

    la ultima

---
+++ components/users/header.styles.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Id = styled.th`    
+        border: 2px dotted purple;    
+`;
+
+export const Avatar = styled.th`    
+        border: 2px solid blue;    
+`;
